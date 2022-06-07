cd658c0d52f6b34f2f2ab973b8097aa1b0f899e6
JoshBenPhilip <joshben4c@gmail.com>
2022-06-07 13:06:18 -0400

    started adding into App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import "./App.css";
+import { Layout } from "antd";
+import { Header } from "antd/lib/layout/layout";
 import HomePage from "./components/HomePage";
+
+const styles = {
+  header: {
+    position: "fixed",
+    zIndex: 10,
+    width: "100%",
+    color: "white",
+  },
+  content: {
+    padding: "0 50px",
+    marginTop: 64,
+  },
+};
 
 export default function App() {
   return (
     <div className="App">
       <h1>Gamified Goals</h1>
-      <Login />
+      <Header>Gamified Goals</Header>
       <HomePage />
-      <EnterNewTask />
-      <CompleteTasK />
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,7 @@
 export default function HomePage() {
   return (
     <>
-      {/* <NavBar /> */}
-      {/* <UserName /> */}
-      {/* <ExpTracker />
-      <Level /> */}
       <Tasks />
-      <CreateTask />
     </>
   );
 }
