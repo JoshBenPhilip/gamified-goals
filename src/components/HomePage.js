d6c8da2836845d4f042ba84372a80b427830888e
JoshBenPhilip <joshben4c@gmail.com>
2022-06-09 10:50:20 -0400

    removed old componenent and refilled HomePage Component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,21 +1,20 @@
-import { useState } from "react";
-import TaskList from "./TaskList";
-import NewTask from "./NewTask";
+import {useState} from "react";
+import TaskList from 
+import NewTask from
 
-export default function HomePage() {
+export default function Homepage() {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState([]);
   return (
-    <section style={{ background: "white", padding: "0 40px 40px" }}>
-      <>this is my boiler plate text</>
+    <section style={{background: "white", padding: "0 40px 40px"}}>
       <TaskList
-        tasks={tasks}
-        setTasks={setTasks}
-        loading={loading}
-        setLoading={setLoading}
+      tasks={tasks}
+      setTasks={setTasks}
+      loading={loading}
+      setLoading={setLoading}
       />
       <br />
       <NewTask setTasks={setTasks} setLoading={setLoading} />
     </section>
-  );
+  )
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,32 +1,3 @@
-import { useEffect } from "react";
-import { List } from "antd";
-import Task from "./Task";
-
-export default function TaskList({ tasks, setTasks, loading, setLoading }) {
-  useEffect(() => {
-    // GET DATA FROM API
-    setLoading(true);
-    fetch("")
-      .then((response) => response.json())
-      .then((data) => {
-        setTasks(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        alert(err);
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <List
-      loading={loading}
-      dataSource={tasks}
-      size="large"
-      bordered
-      renderItem={(item) => (
-        <Task item={item} setLoading={setLoading} setTasks={setTasks} />
-      )}
-    />
-  );
+export default function HomePage() {
+  return <></>;
 }
