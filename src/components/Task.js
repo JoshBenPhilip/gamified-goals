c30e38e71aa3603f4583a8d0aaf0b1890c869b96
JoshBenPhilip <joshben4c@gmail.com>
2022-06-10 18:01:07 -0400

    front end phase working, need to fill in api points

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,3 +1,47 @@
-export default function HomePage() {
-  return <></>;
+import { useState, useEffect } from "react";
+import { List, Checkbox } from "antd";
+
+export default function Task({ item, setTasks, setLoading }) {
+  const [itemStyle, setItemStyle] = useState({});
+  useEffect(() => {
+    if (item.done) {
+      setItemStyle({ color: "grey", textDecoration: "line-through" });
+    } else {
+      setItemStyle({ color: "black", textDecoration: "none" });
+    }
+  }, [item]);
+  const handleToggleTaskDone = () => {
+    setLoading(true);
+    fetch(`mydb/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ done: !item.done }),
+    })
+      .then(() => {
+        // THEN: fetch our tasks
+        fetch("myApiEndpoint/Task")
+          .then((response) => response.json())
+          .then((data) => {
+            setTasks(data);
+            setLoading(false);
+          });
+      })
+      .catch((err) => {
+        alert(err);
+        setLoading(false);
+      });
+  };
+  return (
+    <>
+      <List.Item style={itemStyle}>
+        <Checkbox
+          style={{ margin: "10px" }}
+          onClick={handleToggleTaskDone}
+          checked={item.done}
+        ></Checkbox>
+      </List.Item>
+    </>
+  );
 }
