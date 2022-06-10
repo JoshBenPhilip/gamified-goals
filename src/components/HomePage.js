ee644a2e0364332e584f6f6788b80c7f95353dcd
JoshBenPhilip <joshben4c@gmail.com>
2022-06-10 14:48:48 -0400

    Only Task.js left +db url

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,20 @@
-import {useState} from "react";
-import TaskList from 
-import NewTask from
+import { useState } from "react";
+import TaskList from "./TaskList";
+import NewTask from "./NewTask";
 
 export default function Homepage() {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState([]);
   return (
-    <section style={{background: "white", padding: "0 40px 40px"}}>
+    <section style={{ background: "white", padding: "0 40px 40px" }}>
       <TaskList
-      tasks={tasks}
-      setTasks={setTasks}
-      loading={loading}
-      setLoading={setLoading}
+        tasks={tasks}
+        setTasks={setTasks}
+        loading={loading}
+        setLoading={setLoading}
       />
       <br />
       <NewTask setTasks={setTasks} setLoading={setLoading} />
     </section>
-  )
+  );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,3 +1,32 @@
-export default function HomePage() {
-  return <></>;
+import { useEffect } from "react";
+import { List } from "antd";
+import Task from "./Task.js";
+
+export default function TaskList({ tasks, setTasks, loading, setLoading }) {
+  useEffect(() => {
+    //GET DATA FROM API
+    setLoading(true);
+    fetch("myDbAddressHere")
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        alert(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <List
+      loading={loading}
+      dataSource={tasks}
+      size="large"
+      bordered
+      renderItem={(item) => (
+        <Task item={item} setLoading={setloading} setTasks={setTasks} />
+      )}
+    />
+  );
 }
