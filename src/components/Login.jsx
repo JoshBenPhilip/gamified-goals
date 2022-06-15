298d6097d54340516ddc7901e6340d45e0e48f1e
JoshBenPhilip <joshben4c@gmail.com>
2022-06-15 17:57:07 -0400

    added Login page with some firebase auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,5 +27,41 @@ export default function Login() {
   const { setUser } = useContext(UserContext);
   const handleLogin = ({ email, password }) => {
     const auth = connectAuth();
+    // login with Firebase Auth
+    signInWithEmailAndPassword(auth, email, password)
+      .then((res) => setUser(res.user))
+      .catch(console.error);
   };
+  const handleGoogleLogin = () => {
+    const auth = connectAuth();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((res) => setUser(res.user))
+      .catch(console.error);
+  };
+  return (
+    <section style={{ padding: "2em" }}>
+      <Form
+        onFinish={handleLogin}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please enter a valid email" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
+          <Button onClick={handleGoogleLogin}>Google Login</Button>
+        </Form.Item>
+      </Form>
+    </section>
+  );
 }
