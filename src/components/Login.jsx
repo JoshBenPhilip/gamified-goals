41af16b629df3af84cb9c0782460e8ff3cbfd1cd
JoshBenPhilip <joshben4c@gmail.com>
2022-06-20 11:14:10 -0400

    cool login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -66,6 +66,10 @@ export default function Login() {
           </Button>
         </Form.Item> */}
         <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkF7sfIsCpoazLfaoUolQvPTRAuUwoUo2_RA&usqp=CAU"
+            alt="rpg character cleric"
+          />
           <Button onClick={handleGoogleLogin}>Google Login</Button>
         </Form.Item>
       </Form>
